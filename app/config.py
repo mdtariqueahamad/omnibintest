ddafbc3ca2eff5eb8bc352ea4350813e7cbc7103
Jhakaasjai <namdeomritunjay@gmail.com>
2026-05-13 00:07:57 +0530

    modified:   app/config.py

--- app/config.py
+++ app/config.py
@@ -1,9 +1,22 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
+
 
 class Settings(BaseSettings):
-    # Notice the %40 replacing the @ in the password
-    model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", extra="ignore"
-    )
+
+    mongodb_uri: str = "mongodb://localhost:27017"
+    mongodb_db_name: str = "omnibin"
+
+    mqtt_broker_host: str = "b72ae81547ef42a6b60ddf4d5d6d4a9e.s1.eu.hivemq.cloud"
+    mqtt_broker_port: int = 8883
+    mqtt_topic: str = "omnibin/bins/fill_level"
+
+    mqtt_username: str
+    mqtt_password: str
+
+    openrouter_api_key: str
+
+    class Config:
+        env_file = ".env"
+
 
 settings = Settings()
