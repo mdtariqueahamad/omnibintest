5505f6023ed25532a0e6f998da038f682fc3aac0
mdtariqueahamad <mdtariqbist@gmail.com>
2026-05-13 22:35:19 +0530

    Update mongodb_uri to use correct URI format

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
 
-    mongodb_uri: str = "localhost:27017"
+    mongodb_uri: str = "mongodb://localhost:27017"
     mongodb_db_name: str = "omnibin"
 
     mqtt_broker_host: str = "b72ae81547ef42a6b60ddf4d5d6d4a9e.s1.eu.hivemq.cloud"
